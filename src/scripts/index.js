ca0832f837cc268768681ee1e48b8be2045a69ff
DMawi17 <asegda17@gmail.com>
2021-06-24 16:25:15 +0200

    promised

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,23 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
-import '../styles/main.scss';
+import "../styles/main.scss";
 // Import any additional modules you want to include below \/
-
 
 // \/ All of your javascript should go here \/
 
+const myModal = document.querySelector(".modal");
+
+const promise = new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(), 60000);
+});
+
+promise.then(() => {
+  myModal.style.display = "block";
+});
+
+// ...
+
+const kill = document.querySelector(".close");
+
+kill.addEventListener("click", () => {
+    myModal.style.display = "none";
+});
